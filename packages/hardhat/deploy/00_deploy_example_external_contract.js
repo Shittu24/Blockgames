7e20887d30d36b5b1e5e1cc362148138e731d11e
Shittu24 <ibrahimshittu42@gmail.com>
2022-04-02 23:40:40 -0800

    second commit

--- packages/hardhat/deploy/00_deploy_example_external_contract.js
+++ packages/hardhat/deploy/00_deploy_example_external_contract.js
@@ -17,27 +17,27 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const exampleExternalContract = await ethers.getContract("ExampleExternalContract");
 
   // Getting a previously deployed contract
-  // const ExampleExternalContract = await ethers.getContract(
-  //   "ExampleExternalContract",
-  //   deployer
-  // );
+  const ExampleExternalContract = await ethers.getContract(
+    "ExampleExternalContract",
+    deployer
+  );
 
-  // await YourContract.setPurpose("Hello");
+  await YourContract.setPurpose("Hello");
 
   // if you want to instantiate a version of a contract at a specific address!
   // const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A")
 
   // If you want to send value to an address from the deployer
-  // const deployerWallet = ethers.provider.getSigner()
-  // await deployerWallet.sendTransaction({
-  //   to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
-  //   value: ethers.utils.parseEther("0.001")
-  // })
+  const deployerWallet = ethers.provider.getSigner()
+  await deployerWallet.sendTransaction({
+    to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
+    value: ethers.utils.parseEther("0.001")
+  })
 
   // If you want to send some ETH to a contract on deploy (make your constructor payable!)
-  // const yourContract = await deploy("YourContract", [], {
-  // value: ethers.utils.parseEther("0.05")
-  // });
+  const yourContract = await deploy("YourContract", [], {
+  value: ethers.utils.parseEther("0.05")
+  });
 
   // If you want to link a library into your contract:
   // reference: https://github.com/austintgriffith/scaffold-eth/blob/using-libraries-example/packages/hardhat/scripts/deploy.js#L19
--- packages/hardhat/deploy/01_deploy_staker.js
+++ packages/hardhat/deploy/01_deploy_staker.js
@@ -1,6 +1,6 @@
 // deploy/01_deploy_staker.js
 
-// const { ethers } = require("hardhat");
+const { ethers } = require("hardhat");
 
 module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   const { deploy } = deployments;
@@ -19,26 +19,26 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   });
 
   // Getting a previously deployed contract
-  // const Staker = await ethers.getContract(
-  //   "Staker",
-  //   deployer
-  // );
+  const Staker = await ethers.getContract(
+    "Staker",
+    deployer
+  );
 
-  // await YourContract.setPurpose("Hello");
+    // await YourContract.setPurpose("Hello");
 
-  // const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
+    // const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
 
   // If you want to send value to an address from the deployer
-  // const deployerWallet = ethers.provider.getSigner()
-  // await deployerWallet.sendTransaction({
-  //   to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
-  //   value: ethers.utils.parseEther("0.001")
-  // })
+  const deployerWallet = ethers.provider.getSigner()
+  await deployerWallet.sendTransaction({
+    to: "0x34aA3F359A9D614239015126635CE7732c18fDF3",
+    value: ethers.utils.parseEther("0.001")
+  })
 
   // If you want to send some ETH to a contract on deploy (make your constructor payable!)
-  // const yourContract = await deploy("YourContract", [], {
-  // value: ethers.utils.parseEther("0.05")
-  // });
+  const yourContract = await deploy("YourContract", [], {
+  value: ethers.utils.parseEther("0.05")
+  });
 
   // If you want to link a library into your contract:
   // reference: https://github.com/austintgriffith/scaffold-eth/blob/using-libraries-example/packages/hardhat/scripts/deploy.js#L19

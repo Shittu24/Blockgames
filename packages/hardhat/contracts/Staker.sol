134f7c5cb45d8e2578fd49ce7be8353991da1bd0
Ibrahim Shittu <42889574+Shittu24@users.noreply.github.com>
2022-11-15 21:36:40 +0100

    Update Staker.sol

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -5,88 +5,81 @@ import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 contract Staker {
+    // External contract that will old stacked funds
+    ExampleExternalContract public exampleExternalContract;
 
-  // External contract that will old stacked funds
-  ExampleExternalContract public exampleExternalContract;
+    // Balances of the user's stacked funds
+    mapping(address => uint256) public balances;
 
-  // Balances of the user's stacked funds
-  mapping(address => uint256) public balances;
+    // Staking threshold
+    uint256 public constant threshold = 1 ether;
 
-  // Staking threshold
-  uint256 public constant threshold = 1 ether;
+    // Staking deadline
+    uint256 public deadline = block.timestamp + 72 hours;
 
-  // Staking deadline
-  uint256 public deadline = block.timestamp + 72 hours;
+    // Contract's Events
+    event Stake(address indexed sender, uint256 amount);
 
-  // Contract's Events
-  event Stake(address indexed sender, uint256 amount);
-
-  // Contract's Modifiers
-  modifier deadlineReached( bool requireReached ) {
-    uint256 timeRemaining = timeLeft();
-    if( requireReached ) {
-      require(timeRemaining == 0, "Deadline is not reached yet");
-    } else {
-      require(timeRemaining > 0, "Deadline is already reached");
+    constructor(address exampleExternalContractAddress) {
+        exampleExternalContract = ExampleExternalContract(
+            exampleExternalContractAddress
+        );
     }
-    _;
-  }
 
-  modifier stakeNotCompleted() {
-    bool completed = exampleExternalContract.completed();
-    require(!completed, "staking process already completed");
-    _;
-  }
-  
-
-  constructor(address exampleExternalContractAddress) {
-    exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
-
-  function execute() public stakeNotCompleted deadlineReached(false) {
-    uint256 contractBalance = address(this).balance;
-
-    // check the contract has enough ETH to reach the treshold
-    require(contractBalance >= threshold, "Threshold not reached");
-
-    // Execute the external contract, transfer all the balance to the contract
-    // (bool sent, bytes memory data) = exampleExternalContract.complete{value: contractBalance}();
-    (bool sent,) = address(exampleExternalContract).call{value: contractBalance}(abi.encodeWithSignature("complete()"));
-    require(sent, "exampleExternalContract.complete failed");
-  }
-
-  function stake() public payable deadlineReached(false) stakeNotCompleted {
-    // update the user's balance
-    balances[msg.sender] += msg.value;
-    
-    // emit the event to notify the blockchain that we have correctly Staked some fund for the user
-    emit Stake(msg.sender, msg.value);
-  }
-
-  function withdraw() public deadlineReached(true) stakeNotCompleted {
-    uint256 userBalance = balances[msg.sender];
-
-    // check if the user has balance to withdraw
-    require(userBalance > 0, "You don't have balance to withdraw");
-
-    // reset the balance of the user
-    balances[msg.sender] = 0;
-
-    // Transfer balance back to the user
-    (bool sent,) = msg.sender.call{value: userBalance}("");
-    require(sent, "Failed to send user balance back to the user");
-  }
-
-  function timeLeft() public view returns (uint256 timeleft) {
-    if( block.timestamp >= deadline ) {
-      return 0;
-    } else {
-      return deadline - block.timestamp;
+    // Contract's modifiers
+    modifier stakeNotCompleted() {
+        bool completed = exampleExternalContract.completed();
+        require(!completed, "staking process already completed");
+        _;
     }
-  }
 
-  receive() external payable {
-    return stake();
-  }
+    function execute() public stakeNotCompleted {
+        uint256 contractBalance = address(this).balance;
 
+        // check the contract has enough ETH to reach the treshold
+        //  require(contractBalance >= threshold, "Threshold not reached");
+
+        if (contractBalance >= threshold) {
+            (bool sent, ) = address(exampleExternalContract).call{
+                value: contractBalance
+            }(abi.encodeWithSignature("complete()"));
+            require(sent, "exampleExternalContract.complete failed");
+        } else {
+            withdraw();
+        }
+    }
+
+    function stake() public payable {
+        // update the user's balance
+        balances[msg.sender] += msg.value;
+
+        // emit the event to notify the blockchain that we have correctly Staked some fund for the user
+        emit Stake(msg.sender, msg.value);
+    }
+
+    function withdraw() public stakeNotCompleted {
+        uint256 userBalance = balances[msg.sender];
+
+        // check if the user has balance to withdraw
+        require(userBalance >= 0, "You don't have balance to withdraw");
+
+        // reset the balance of the user
+        balances[msg.sender] = 0;
+
+        // Transfer balance back to the user
+        (bool sent, ) = msg.sender.call{value: userBalance}("");
+        require(sent, "Failed to send user balance back to the user");
+    }
+
+    function timeLeft() public view returns (uint256 timeleft) {
+        if (block.timestamp >= deadline) {
+            return 0;
+        } else {
+            return deadline - block.timestamp;
+        }
+    }
+
+    receive() external payable {
+        return stake();
+    }
 }
